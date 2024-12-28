cf1ae28a2c92ad58fb40810952fc1d06ced4c24b
Kureichi <165907455+Kuredew@users.noreply.github.com>
2024-12-29 02:28:50 +0700

    Update betterUI.js

--- betterUI.js
+++ betterUI.js
@@ -35,7 +35,7 @@ if (!id_user) {
     var id = generateUUID();
     //var id = crypto.randomUUID();
     localStorage.setItem('id_user', id);
-    var id_user = id;
+    id_user = id;
 }
 
 console.log(id_user);
